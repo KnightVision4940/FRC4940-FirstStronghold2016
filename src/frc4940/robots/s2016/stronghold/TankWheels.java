06f93beb26c5143b90cd1302f2dad3b0fadda261
Stew Esho <StewardEsho@gmail.com>
2016-01-23 16:47:43 -0500

    Update TankWheels.java

--- src/frc4940/robots/s2016/stronghold/TankWheels.java
+++ src/frc4940/robots/s2016/stronghold/TankWheels.java
@@ -5,10 +5,10 @@ public class TankWheels {
 	
 	private final int LEFTFRONTWHEEL = 1;
 	private final int RIGHTFRONTWHEEL = 2;
-	private final int	LEFTBACKWHEEL = 3;
-	private final int	RIGHTBACKWHEEL = 4;
+	private final int LEFTBACKWHEEL = 3;
+	private final int RIGHTBACKWHEEL = 4;
 	
-	RobotDrive Wheels = new RobotDrive(Map.LEFTBACKWHEEL,Map.LEFTFRONTWHEEL,Map.RIGHTBACKWHEEL,Map.RIGHTFRONTWHEEL);
+	RobotDrive Wheels = new RobotDrive(LEFTBACKWHEEL, LEFTFRONTWHEEL, RIGHTBACKWHEEL, RIGHTFRONTWHEEL);
 	
 	public void DriveRobot(double _Speed,double _Turn){
 		Wheels.drive(_Speed, _Turn);
