08de9ccb39f87265926991daa885cb3042a3e9cf
Stew Esho <StewardEsho@gmail.com>
2016-02-08 17:13:05 -0500

    Removed DIO code (since we have no switches)

--- src/frc4940/robots/s2016/stronghold/IO.java
+++ src/frc4940/robots/s2016/stronghold/IO.java
@@ -5,17 +5,6 @@ import edu.wpi.first.wpilibj.Joystick;
 public class IO {
 	
 	static Joystick xbox = new Joystick(0);
-
-	static DigitalInput LowerArm = new DigitalInput(Map.LOWERARM);
-	static DigitalInput UpperArm = new DigitalInput(Map.UPPERARM);
-	
-	public static boolean GetLowerArm(){
-		return LowerArm.get();
-	}
-	
-	public static boolean GetUpperArm(){
-		return UpperArm.get();
-	}
 	
 	public static double getXboxLeftX(){
 		return xbox.getRawAxis(0);
