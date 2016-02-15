0eef1c7be01ba393e236968f534f95ba00f24aa2
Stew Esho <StewardEsho@gmail.com>
2016-02-15 10:32:56 -0500

    Added Arm code [CAN]

--- src/frc4940/robots/s2016/stronghold/Arm.java
+++ src/frc4940/robots/s2016/stronghold/Arm.java
@@ -1,10 +1,10 @@
 package frc4940.robots.s2016.stronghold;
-import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 
 public class Arm {
 		
-	VictorSP __Arm = new VictorSP(Map.PWM.ARM_);
+	CANTalon __Arm = new CANTalon(Map.CAN.ARM_);
 	
 	public double GetArm(){
 		return __Arm.get();
@@ -12,6 +12,12 @@ public class Arm {
 
 	public void SetArm(double _Speed){
 		__Arm.set(_Speed);
+	}
+	
+	public int getArmPosition(){
+		System.out.println(__Arm.getEncPosition());
+		System.out.println("\n");
+		return __Arm.getEncPosition();
 	}
 	
 }
